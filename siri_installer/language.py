c26034536b58abfcc192edc14c43260346f8d595
Erdem Üveyik <73111211+ErdemBey1@users.noreply.github.com>
2021-02-11 18:29:14 +0300

    Update language.py

--- siri_installer/language.py
+++ siri_installer/language.py
@@ -28,4 +28,4 @@ def importlang ():
     return COUNTRY, LANGUAGE, TZ
 
 COUNTRY, LANGUAGE, TZ = importlang()
-LANG = loads(open(f"./siri_installer/language/{LANGUAGE}.sirijson", "r").read())["STRINGS"]
+LANG = loads(open(f"./siri_installer/language/{LANGUAGE}.spacejson", "r").read())["STRINGS"]
