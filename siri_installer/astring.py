f6c36999886fa177e545966a63635283c9a28ae6
Erdem Bey <73111211+ErdemBey1@users.noreply.github.com>
2021-03-01 16:36:27 +0300

    Update astring.py

--- siri_installer/astring.py
+++ siri_installer/astring.py
@@ -114,7 +114,7 @@ def main():
                 app_title = choice(["as", "ase", "asen", "madelineproto", "telethon", "pyrogram"]) + choice(["", "-", "+", " "]) + choice(["user", "bot", "vue", "jsx", "python", "php"]) + choice([str(randint(10000, 99999)), ""])
             app_shortname = soru("APPın kısa adı ne olsun? (Otomatik Oluşturmak için enter tuşuna basın) \[5-32 karakter\]: ")
             if app_shortname == '':
-                app_title = choice(["as", "ase", "asen", "madelineproto", "telethon", "pyrogram"]) + choice(["", "-", "+", " "]) + choice(["user", "bot", "vue", "jsx", "python", "php"]) + choice([str(randint(10000, 99999)), ""])
+                app_shortname = choice(["as", "ase", "asen", "madelineproto", "telethon", "pyrogram"]) + choice(["", "-", "+", " "]) + choice(["user", "bot", "vue", "jsx", "python", "php"]) + choice([str(randint(10000, 99999)), ""])
             AppInfo = {
                 "hash": hashh,
                 "app_title": app_title,
